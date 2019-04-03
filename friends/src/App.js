f954058b5d41d84dd3826f11a8ec2f0942bc9c03
unknown <lappjeff20@gmail.com>
2019-04-03 15:33:44 -0400

    Created app structure and removed unnecessary items

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, useState, useEffect } from 'react';
+import './App.css';
+
+const App = props => {
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+  
+}
+
+export default App;
